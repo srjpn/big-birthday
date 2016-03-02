f639f034f0b357ee5fdfa2e39f773027d31121b1
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-03-02 20:25:42 +0530

    Modified MailingAddress Class
    Modified MailingAddress class to make the fields public so that the
    MailTemplate can access it.

--- src/MailingAddress.java
+++ src/MailingAddress.java
@@ -1,7 +1,7 @@
 public class MailingAddress {
-    private final Name name;
-    private final Gender gender;
-    private final Address address;
+    final Name name;
+    final Gender gender;
+    final Address address;
 
     public MailingAddress(Name name, Gender gender, Address address) {
         this.name = name;
@@ -14,6 +14,6 @@ public class MailingAddress {
         String initial = this.gender.getInitial();
         String name = this.name.toString();
         String address = this.address.toString();
-        return initial+name+"\n"+address;
+        return initial+". "+name+"\n"+address;
     }
 }
